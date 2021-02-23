7a1f115b657e2da10ee3fea9af330f256a9cf202
Ronny <ronnykuruvilla@gmail.com>
2021-02-23 15:19:27 -0500

    code along.

--- fizzy.js
+++ fizzy.js
@@ -1,1 +1,17 @@
 const getInput = require("./get-input.js");
+const fizzy = require("./fizzy-backend.js");
+
+const n = getInput();
+const answer = fizzy(n);
+console.log(answer);
+
+if(n % 5 === 0 && n % 3 === 0){
+    console.log('FizzBuzz');
+}else if (n % 3 === 0) {
+    console.log("Fizz");
+}else if (num % 5 === 0){
+    return "Buzz";
+}else {
+    console.log(n);
+}
+
--- mental-age.js
+++ mental-age.js
@@ -1,1 +1,23 @@
 const getInput = require("./get-input.js");
+
+
+// 1. take in our user's age, saving it in a variable
+const userAge = getInput();
+
+// 2. check if they want help
+if (userAge === '--help'){
+    console.log('This function returns your mental age.');
+    console.log('Please enter your age after "node mental-age.js" and a space');
+}
+
+// 3. divide it by 2 and add 3
+const mentalAge = userAge / 2 + 3;
+const ageNextYear = Number(userAge) + 1;
+
+// 4. print out the user's age in a sentence
+// For example: ""
+console.log('You are ' + userAge + ' years old.');
+console.log("Next year you'll be " + ageNextYear + " years old.");
+
+// 5. print out the result
+console.log('Your mental age is ' + mentalAge + ' years old.');
